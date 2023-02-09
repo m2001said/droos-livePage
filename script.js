8eee081fb1cc8dea6764417073a4d7572565d3f4
Mohamed Said <ms277712@gmail.com>
2023-02-09 23:25:04 +0200

    modify content of live-info

---
+++ script.js
@@ -0,0 +1,9 @@
+// const btns = document.querySelectorAll(".live-btn");
+// btns.forEach((btn) => {
+//   const infoBox = document.querySelector(".live-info");
+//   btn.addEventListener("click", () => {
+//     infoBox.style.display = "block";
+//     btn.classList.add("active");
+//   });
+//   btn.classList.remove("active");
+// });
