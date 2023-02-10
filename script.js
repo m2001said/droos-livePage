6fa6761482794dab463449f2f34e6b079e09002c
Mohamed Said <ms277712@gmail.com>
2023-02-10 15:06:44 +0200

    modifing sharing button in all divs

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const box1 = document.querySelector("#live-box-1");
 const box2 = document.querySelector("#live-box-2");
 const box3 = document.querySelector("#live-box-3");
 
+const shareBtns = document.querySelectorAll(".live-share-btn");
+const resultShares = document.querySelectorAll(".live-share-result");
+
 const showDiv = (id) => {
   box1.style.display = "none";
   box2.style.display = "none";
@@ -21,20 +24,30 @@ const activeBtn = (className) => {
   document.querySelector(className).classList.add("active");
 };
 
+const deleteResultShare = () =>
+  resultShares.forEach((resultShare) => {
+    resultShare.textContent = "";
+  });
+
 btn1.addEventListener("click", () => {
   showDiv("live-box-1");
   activeBtn(".live-btn-1");
   window.history.pushState({}, "", "#live-box-1");
+  deleteResultShare();
 });
+
 btn2.addEventListener("click", () => {
   showDiv("live-box-2");
   activeBtn(".live-btn-2");
   window.history.pushState({}, "", "#live-box-2");
+  deleteResultShare();
 });
+
 btn3.addEventListener("click", () => {
   showDiv("live-box-3");
   activeBtn(".live-btn-3");
   window.history.pushState({}, "", "#live-box-3");
+  deleteResultShare();
 });
 
 //when the page is loaded
@@ -42,28 +55,31 @@ const hash = window.location.hash.substr(1);
 const idNumber = hash.slice(-1);
 if (hash) {
   showDiv(hash);
-  for (let i = 1; i < 3; i++) {
+  for (let i = 1; i < 4; i++) {
     if (idNumber == i) {
       activeBtn(`.live-btn-${i}`);
     }
   }
 }
 // --------------------sharing buttons
-const shareBtn = document.querySelector(".live-share-btn");
-const resultShare = document.querySelector(".live-share-result");
-const urlPage = window.location.href;
 
 const shareData = {
   title: "موقع دروس الامارات",
   text: "لقد أعجبني هذا الدرس فلعله ينفعكم ",
-  url: urlPage,
+  url: window.location.href,
 };
 
-shareBtn.addEventListener("click", async () => {
-  try {
-    await navigator.share(shareData);
-    resultShare.textContent = "تم المشاركة بنجاح";
-  } catch (err) {
-    resultShare.textContent = "نأسف لك لم تتم المشاركة بنجاح";
-  }
+shareBtns.forEach((btn) => {
+  btn.addEventListener("click", async () => {
+    try {
+      await navigator.share(shareData);
+      resultShares.forEach((resultShare) => {
+        resultShare.textContent = "تم المشاركة بنجاح";
+      });
+    } catch (err) {
+      resultShares.forEach((resultShare) => {
+        resultShare.textContent = "نأسف لك لم تتم المشاركة بنجاح";
+      });
+    }
+  });
 });
